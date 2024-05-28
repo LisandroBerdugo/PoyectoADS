73c31095e99fc0673a8ef911727d6be2ab5c6a24
Lisandro Berdugo <lisanro1995@gmail.com>
2024-05-27 23:40:30 -0600

    cambios 27052024 2340

--- Prueba2 Fase1/FrmPrueba6.Designer.cs
+++ Prueba2 Fase1/FrmPrueba6.Designer.cs
@@ -4,6 +4,9 @@
     {
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.Button btnGenerarReporte;
+        private System.Windows.Forms.Button btnGenerarReporteCompras;
+        private System.Windows.Forms.Button btnGenerarReporteVentas;
+        private System.Windows.Forms.ProgressBar progressBar;
 
         protected override void Dispose(bool disposing)
         {
@@ -17,6 +20,9 @@
         private void InitializeComponent()
         {
             this.btnGenerarReporte = new System.Windows.Forms.Button();
+            this.btnGenerarReporteCompras = new System.Windows.Forms.Button();
+            this.btnGenerarReporteVentas = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // btnGenerarReporte
@@ -25,16 +31,46 @@
             this.btnGenerarReporte.Name = "btnGenerarReporte";
             this.btnGenerarReporte.Size = new System.Drawing.Size(150, 23);
             this.btnGenerarReporte.TabIndex = 0;
-            this.btnGenerarReporte.Text = "Generar Reporte";
+            this.btnGenerarReporte.Text = "Generar Reporte Balance";
             this.btnGenerarReporte.UseVisualStyleBackColor = true;
             this.btnGenerarReporte.Click += new System.EventHandler(this.BtnGenerarReporte_Click);
             // 
+            // btnGenerarReporteCompras
+            // 
+            this.btnGenerarReporteCompras.Location = new System.Drawing.Point(12, 41);
+            this.btnGenerarReporteCompras.Name = "btnGenerarReporteCompras";
+            this.btnGenerarReporteCompras.Size = new System.Drawing.Size(150, 23);
+            this.btnGenerarReporteCompras.TabIndex = 1;
+            this.btnGenerarReporteCompras.Text = "Generar Reporte Compras";
+            this.btnGenerarReporteCompras.UseVisualStyleBackColor = true;
+            this.btnGenerarReporteCompras.Click += new System.EventHandler(this.BtnGenerarReporteCompras_Click);
+            // 
+            // btnGenerarReporteVentas
+            // 
+            this.btnGenerarReporteVentas.Location = new System.Drawing.Point(12, 70);
+            this.btnGenerarReporteVentas.Name = "btnGenerarReporteVentas";
+            this.btnGenerarReporteVentas.Size = new System.Drawing.Size(150, 23);
+            this.btnGenerarReporteVentas.TabIndex = 2;
+            this.btnGenerarReporteVentas.Text = "Generar Reporte Ventas";
+            this.btnGenerarReporteVentas.UseVisualStyleBackColor = true;
+            this.btnGenerarReporteVentas.Click += new System.EventHandler(this.BtnGenerarReporteVentas_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 100);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(260, 23);
+            this.progressBar.TabIndex = 3;
+            // 
             // FrmPrueba6
             // 
-            this.ClientSize = new System.Drawing.Size(284, 61);
+            this.ClientSize = new System.Drawing.Size(284, 136);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnGenerarReporteVentas);
+            this.Controls.Add(this.btnGenerarReporteCompras);
             this.Controls.Add(this.btnGenerarReporte);
             this.Name = "FrmPrueba6";
-            this.Text = "Generar Reporte de Balance";
+            this.Text = "Generar Reportes";
             this.ResumeLayout(false);
         }
     }
--- Prueba2 Fase1/FrmPrueba6.cs
+++ Prueba2 Fase1/FrmPrueba6.cs
@@ -27,6 +27,11 @@ namespace Prueba2_Fase1
             List<ComprasEL> compras = comprasDal.ObtenerCompras();
             List<VentasEL> ventas = ventasDal.ObtenerVentas();
 
+            int totalSteps = compras.Count + ventas.Count + 5;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = totalSteps;
+            progressBar.Value = 0;
+
             string path = "ReporteBalance.pdf";
             using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None))
             {
@@ -34,11 +39,10 @@ namespace Prueba2_Fase1
                 PdfWriter writer = PdfWriter.GetInstance(document, fs);
                 document.Open();
 
-                // Título del documento
                 document.Add(new Paragraph("Reporte de Balance de Compras y Ventas"));
                 document.Add(new Paragraph(" "));
+                UpdateProgressBar();
 
-                // Sección de Compras
                 document.Add(new Paragraph("Compras"));
                 PdfPTable tableCompras = new PdfPTable(4);
                 tableCompras.AddCell("ID");
@@ -55,20 +59,18 @@ namespace Prueba2_Fase1
                     tableCompras.AddCell(compra.FechaCompra.ToString("dd/MM/yyyy"));
                     tableCompras.AddCell(totalCompra.ToString("C2"));
                     totalCompras += totalCompra;
+                    UpdateProgressBar();
                 }
 
-                // Agregar total de compras
                 tableCompras.AddCell("");
                 tableCompras.AddCell("");
                 tableCompras.AddCell("Total Compras");
                 tableCompras.AddCell(totalCompras.ToString("C2"));
-
                 document.Add(tableCompras);
 
-                // Espacio entre tablas
                 document.Add(new Paragraph(" "));
+                UpdateProgressBar();
 
-                // Sección de Ventas
                 document.Add(new Paragraph("Ventas"));
                 PdfPTable tableVentas = new PdfPTable(4);
                 tableVentas.AddCell("ID");
@@ -85,28 +87,142 @@ namespace Prueba2_Fase1
                     tableVentas.AddCell(venta.FechaVenta.ToString("dd/MM/yyyy"));
                     tableVentas.AddCell(totalVenta.ToString("C2"));
                     totalVentas += totalVenta;
+                    UpdateProgressBar();
                 }
 
-                // Agregar total de ventas
                 tableVentas.AddCell("");
                 tableVentas.AddCell("");
                 tableVentas.AddCell("Total Ventas");
                 tableVentas.AddCell(totalVentas.ToString("C2"));
-
                 document.Add(tableVentas);
 
-                // Espacio entre tablas
                 document.Add(new Paragraph(" "));
+                UpdateProgressBar();
 
-                // Total General
                 decimal totalGeneral = totalVentas - totalCompras;
                 document.Add(new Paragraph($"Total General (Ventas - Compras): {totalGeneral:C2}"));
 
                 document.Close();
                 writer.Close();
+                UpdateProgressBar();
             }
 
             MessageBox.Show("Reporte generado con éxito en " + Path.GetFullPath(path), "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            progressBar.Value = 0;
+        }
+
+        private void BtnGenerarReporteCompras_Click(object sender, EventArgs e)
+        {
+            List<ComprasEL> compras = comprasDal.ObtenerCompras();
+
+            int totalSteps = compras.Count + 3;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = totalSteps;
+            progressBar.Value = 0;
+
+            string path = "ReporteCompras.pdf";
+            using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                Document document = new Document();
+                PdfWriter writer = PdfWriter.GetInstance(document, fs);
+                document.Open();
+
+                document.Add(new Paragraph("Reporte de Compras"));
+                document.Add(new Paragraph(" "));
+                UpdateProgressBar();
+
+                PdfPTable tableCompras = new PdfPTable(4);
+                tableCompras.AddCell("ID");
+                tableCompras.AddCell("Proveedor");
+                tableCompras.AddCell("Fecha");
+                tableCompras.AddCell("Total");
+
+                decimal totalCompras = 0;
+                foreach (var compra in compras)
+                {
+                    decimal totalCompra = compra.Detalles.Sum(d => d.Cantidad * d.PrecioCompra);
+                    tableCompras.AddCell(compra.ID.ToString());
+                    tableCompras.AddCell(compra.ProveedorNombre);
+                    tableCompras.AddCell(compra.FechaCompra.ToString("dd/MM/yyyy"));
+                    tableCompras.AddCell(totalCompra.ToString("C2"));
+                    totalCompras += totalCompra;
+                    UpdateProgressBar();
+                }
+
+                tableCompras.AddCell("");
+                tableCompras.AddCell("");
+                tableCompras.AddCell("Total Compras");
+                tableCompras.AddCell(totalCompras.ToString("C2"));
+                document.Add(tableCompras);
+
+                document.Close();
+                writer.Close();
+                UpdateProgressBar();
+            }
+
+            MessageBox.Show("Reporte de compras generado con éxito en " + Path.GetFullPath(path), "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            progressBar.Value = 0;
+        }
+
+        private void BtnGenerarReporteVentas_Click(object sender, EventArgs e)
+        {
+            List<VentasEL> ventas = ventasDal.ObtenerVentas();
+
+            int totalSteps = ventas.Count + 3;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = totalSteps;
+            progressBar.Value = 0;
+
+            string path = "ReporteVentas.pdf";
+            using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                Document document = new Document();
+                PdfWriter writer = PdfWriter.GetInstance(document, fs);
+                document.Open();
+
+                document.Add(new Paragraph("Reporte de Ventas"));
+                document.Add(new Paragraph(" "));
+                UpdateProgressBar();
+
+                PdfPTable tableVentas = new PdfPTable(4);
+                tableVentas.AddCell("ID");
+                tableVentas.AddCell("Cliente");
+                tableVentas.AddCell("Fecha");
+                tableVentas.AddCell("Total");
+
+                decimal totalVentas = 0;
+                foreach (var venta in ventas)
+                {
+                    decimal totalVenta = venta.Detalles.Sum(d => d.Cantidad * d.PrecioVenta);
+                    tableVentas.AddCell(venta.ID.ToString());
+                    tableVentas.AddCell($"{venta.ClienteNombre} {venta.ClienteApellido}");
+                    tableVentas.AddCell(venta.FechaVenta.ToString("dd/MM/yyyy"));
+                    tableVentas.AddCell(totalVenta.ToString("C2"));
+                    totalVentas += totalVenta;
+                    UpdateProgressBar();
+                }
+
+                tableVentas.AddCell("");
+                tableVentas.AddCell("");
+                tableVentas.AddCell("Total Ventas");
+                tableVentas.AddCell(totalVentas.ToString("C2"));
+                document.Add(tableVentas);
+
+                document.Close();
+                writer.Close();
+                UpdateProgressBar();
+            }
+
+            MessageBox.Show("Reporte de ventas generado con éxito en " + Path.GetFullPath(path), "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            progressBar.Value = 0;
+        }
+
+        private void UpdateProgressBar()
+        {
+            if (progressBar.Value < progressBar.Maximum)
+            {
+                progressBar.Value++;
+            }
         }
     }
 }
